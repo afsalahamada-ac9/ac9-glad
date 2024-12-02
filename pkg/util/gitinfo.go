33ea40bf22207e8b50c45f7546618b72e0dcf9fb
Sudhagar Chinnaswamy <sudhagar@abovecloud9.ai>
2024-12-02 23:41:14 +0530

    update gitinfo

--- pkg/util/gitinfo.go
+++ pkg/util/gitinfo.go
@@ -1,8 +1,8 @@
 package util
 
 var (
-    GitTag     = "v0.0.1-5-g57de20f"
-    GitHash    = "57de20f"
+    GitTag     = "v0.0.1-6-g33b2db7"
+    GitHash    = "33b2db7"
     BuildDate  = "2024-12-02"
-    BuildTime  = "18:10:28"
+    BuildTime  = "18:11:14"
 )
